fa97d16f84a0f61de8921cabd5c5e6971234101b
Yahya <yahyahussein91@gmail.com>
2025-07-22 22:51:54 -0700

    added a wait and close browser

--- day03_locate_by_tag_get_attr/find_element_by_tag_name.py
+++ day03_locate_by_tag_get_attr/find_element_by_tag_name.py
@@ -1,5 +1,7 @@
-from selenium import webdriver
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
@@ -7,3 +9,7 @@ driver.get("https://practice.cydeo.com")
 
 first_link = driver.find_element(By.TAG_NAME, "a")
 print(first_link.text)
+
+time.sleep(2)
+
+driver.quit()

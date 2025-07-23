1bd0465bf62bd8cfac3bb47ba5cdcf1f233a03b8
Yahya <yahyahussein91@gmail.com>
2025-07-22 22:50:52 -0700

    getting text of first link

--- day03_locate_by_tag_get_attr/find_element_by_tag_name.py
+++ day03_locate_by_tag_get_attr/find_element_by_tag_name.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 
+
 driver = webdriver.Chrome()
 
 driver.get("https://practice.cydeo.com")
+
+first_link = driver.find_element(By.TAG_NAME, "a")
+print(first_link.text)

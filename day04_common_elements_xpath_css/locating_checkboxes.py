efbe9a3de435bdbcda58fc2010b1dccfe8021873
yahya09206 <yahyahussein91@gmail.com>
2025-07-25 08:37:26 -0700

    more scenarios with checkboxes

--- day04_common_elements_xpath_css/locating_checkboxes.py
+++ day04_common_elements_xpath_css/locating_checkboxes.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -9,3 +11,34 @@ driver.get("https://practice.cydeo.com/checkboxes")
 checkbox_one = driver.find_element(By.ID,"box1")
 print(checkbox_one.is_selected())
 checkbox_one.get_attribute("checked")
+
+# Identify second checkbox
+checkbox_two = driver.find_element(By.ID,"box2")
+print(checkbox_two.is_selected())
+checkbox_two.get_attribute("checked")
+
+# Click box_one to check it
+checkbox_one.click()
+print(checkbox_one.is_selected())
+# Click box_two to uncheck it
+checkbox_two.click()
+print(checkbox_two.is_selected())
+
+# If box one is not selected, then check it
+# Else, display "already checked"
+if checkbox_one is not checkbox_one.is_selected():
+    checkbox_one.click()
+else:
+    print("Take it easy, already checked")
+
+
+# If box two is not selected then check
+# Else, display "already checked"
+if checkbox_two is not checkbox_two.is_selected():
+    checkbox_two.click()
+else:
+    print("Take it easy, already checked")
+
+time.sleep(3)
+
+driver.quit()

525663ddfde2f7455842d1ffba9d3b8116310abd
yahya09206 <yahyahussein91@gmail.com>
2025-07-25 08:16:36 -0700

    added waits and driver quit method

--- day04_common_elements_xpath_css/find_element_by_css_selector.py
+++ day04_common_elements_xpath_css/find_element_by_css_selector.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -5,3 +7,21 @@ driver = webdriver.Chrome()
 
 driver.get("https://search.yahoo.com")
 
+# Identify yahoo search box using CssSelector
+
+# Can't get text using cssSelector
+
+# Wait a little bit
+time.sleep(3)
+
+# Clear text from searchBox
+
+# Type in XPATH into searchBox
+
+# Click submit
+
+# Wait a little bit
+time.sleep()
+
+# quit driver
+driver.quit()

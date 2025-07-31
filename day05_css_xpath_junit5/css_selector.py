76ae57dc8635436ead8164293dd3bcfe0cd6a8ef
yahya09206 <yahyahussein91@gmail.com>
2025-07-30 22:17:32 -0700

    located email box by css selector

--- day05_css_xpath_junit5/css_selector.py
+++ day05_css_xpath_junit5/css_selector.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+
+# navigate to practice site's forgot_password page
+driver.get("https://practice.cydeo.com/forgot_password")
+
+
+# locate two elements using css selector and enter email
+email_box = driver.find_element(By.CSS_SELECTOR, "input[type='text']")
+email_box.send_keys("someone@someone.com")

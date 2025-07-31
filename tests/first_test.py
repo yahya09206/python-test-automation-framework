c68a1f6ac1bda31f36aec424099b50d788ef4b04
yahya09206 <yahyahussein91@gmail.com>
2025-07-30 22:34:51 -0700

    trying to assert title

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,4 +1,5 @@
-from my_first_test import add, subtract
+from day03_locate_by_tag_get_attr.get_elements_by_class_name import driver
+from my_first_test import add, subtract, assertTitle
 
 def test_add_positive_numbers():
     assert add(2, 3) == 5
@@ -8,3 +9,8 @@ def test_add_negative_numbers():
 
 def test_subtract_numbers():
     assert subtract(10, 4) == 6
+
+def test_title_name():
+    title = driver.get("https://practice.cydeo.com")
+
+    assert driver.title(title)
--- tests/my_first_test.py
+++ tests/my_first_test.py
@@ -1,5 +1,11 @@
+from day03_locate_by_tag_get_attr.get_elements_by_class_name import driver
+
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+
+def assertTitle():
+    driver.title("Practice")

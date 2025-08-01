4e59900edc5bcb87bb7e750f8d2a0717c0478b5e
yahya09206 <yahyahussein91@gmail.com>
2025-07-31 19:13:55 -0700

    set scope for driver

--- tests/test_title.py
+++ tests/test_title.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+@pytest.fixture(scope="module")
 def driver():
     # headless driver options
     options = Options()

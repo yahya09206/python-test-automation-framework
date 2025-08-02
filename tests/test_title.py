7b346a2c081e1f3103d0942075d65c5b13d84d0d
yahya09206 <yahyahussein91@gmail.com>
2025-08-01 18:00:44 -0700

    forgot to import commons library

--- tests/test_title.py
+++ tests/test_title.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="module")
@@ -32,6 +33,4 @@ def test_yahoo_title(driver):
 
 def test_search_page_title(driver):
 
-
-
-
+    search_box = driver.find_element(By.)

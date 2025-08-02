d712017325565ce282af05409a7bfc6f33989734
yahya09206 <yahyahussein91@gmail.com>
2025-08-01 18:16:43 -0700

    checking for existing chrome driver

--- utils/driver.py
+++ utils/driver.py
@@ -5,9 +5,19 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import os
 
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 class Driver:
 
     _driver = None
 
     @classmethod
     def get_driver(cls):
+        browser_name = os.getenv("BROWSER", "chrome").lower()
+
+        if cls._driver is None:
+            if browser_name == "chrome":
+                cls._driver = webdriver.Chrome(
+                    service=ChromeService()
+                )

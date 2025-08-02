c0128bf97fbc5ac0bac0307ce82b4636a2aaa527
yahya09206 <yahyahussein91@gmail.com>
2025-08-01 18:10:40 -0700

    setting up driver class as singleton object

---
+++ utils/driver.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import os

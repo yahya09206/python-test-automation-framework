3d89e35186aeb87259e38bed4e37c963915cb1a0
yahya09206 <yahyahussein91@gmail.com>
2025-08-07 19:04:47 -0700

    added option for headless driver

--- tests/day06_pytest_utility_methods/is_displayed_test.py
+++ tests/day06_pytest_utility_methods/is_displayed_test.py
@@ -1,1 +1,10 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="module")
+def driver()
+    # headless driver options
+    options = Options()
+    options.add_argument("--headless")

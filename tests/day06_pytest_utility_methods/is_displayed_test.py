0a3ca140ed31da9e43bfc062da1fe589b8bb1af3
yahya09206 <yahyahussein91@gmail.com>
2025-08-07 19:24:17 -0700

    verified test for error message is working as expected

--- tests/day06_pytest_utility_methods/is_displayed_test.py
+++ tests/day06_pytest_utility_methods/is_displayed_test.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,8 +9,8 @@ from selenium.webdriver.common.by import By
 @pytest.fixture(scope="module")
 def driver():
     # headless driver options
-    # options = Options()
-    # options.add_argument("--headless")
+    options = Options()
+    options.add_argument("--headless")
 
     # set up webdriver
     driver = webdriver.Chrome()
@@ -21,8 +23,14 @@ def test_error_message(driver):
     # xpath for error message
     x_path_for_error_message = "//small[.='first name must be more than 2 and less than 64 characters long']"
 
-    # Error message will only be displayed after user attempts to login with 2 or less characters
+    # Error message will only be displayed after user attempts to log in with 2 or fewer characters
     string_error_element = driver.find_element(By.XPATH,x_path_for_error_message)
+    print(string_error_element.is_displayed())
+    # assert string_error_element.is_displayed()
+
+    first_name_field = driver.find_element(By.NAME, "firstname")
+    first_name_field.send_keys("a")
+
     print(string_error_element.is_displayed())
     assert string_error_element.is_displayed()
 

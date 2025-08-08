6246c8115a1cad26c42b79a01d0132a71ccad0a1
yahya09206 <yahyahussein91@gmail.com>
2025-08-07 19:11:08 -0700

    fixed approach

--- tests/day06_pytest_utility_methods/is_displayed_test.py
+++ tests/day06_pytest_utility_methods/is_displayed_test.py
@@ -1,16 +1,25 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture(scope="module")
 def driver():
     # headless driver options
-    options = Options()
-    options.add_argument("--headless")
+    # options = Options()
+    # options.add_argument("--headless")
 
     # set up webdriver
     driver = webdriver.Chrome()
     driver.get("https://practice.cydeo.com/registration_form")
     yield driver
     driver.quit()
+
+def test_error_message(driver):
+
+    # xpath for error message
+    x_path_for_error_message = "//small[.='first name must be more than 2 and less than 64 characters long']"
+
+
+

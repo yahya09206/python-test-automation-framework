6bc9c141b9ea8f03a5288e42f109031f508415bc
yahya09206 <yahyahussein91@gmail.com>
2025-08-11 18:14:37 -0700

    navigating to site

--- tests/day06_pytest_utility_methods/login_form_test.py
+++ tests/day06_pytest_utility_methods/login_form_test.py
@@ -4,3 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 @pytest.fixture(scope="module")
+def driver():
+
+    driver = webdriver.Chrome()
+    driver.get("https://practice.cydeo.com/login")

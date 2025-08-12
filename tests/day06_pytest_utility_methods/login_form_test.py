f8dab9b1012b2229963148257225579af0896130
yahya09206 <yahyahussein91@gmail.com>
2025-08-11 18:21:19 -0700

    enter credentials for login form

--- tests/day06_pytest_utility_methods/login_form_test.py
+++ tests/day06_pytest_utility_methods/login_form_test.py
@@ -8,3 +8,22 @@ def driver():
 
     driver = webdriver.Chrome()
     driver.get("https://practice.cydeo.com/login")
+    yield driver
+    driver.quit()
+
+def test_login(driver):
+
+    # locate header element
+    header = driver.find_element(By.XPATH, "//h2[.='Login Page']")
+    # get header element text
+    print(header.text)
+
+    # enter credentials into form
+    driver.find_element(By.XPATH, "//div/input[@name='username']").send_keys("tomsmith")
+    driver.find_element(By.XPATH, "//div/input[@name='password']").send_keys("SuperSecretPassword")
+    driver.find_element(By.ID, "wooden_spoon").click()
+
+
+
+
+

18500efc7704b563fd6ad5265d41691b572a5974
yahya09206 <yahyahussein91@gmail.com>
2025-08-18 18:21:08 -0700

    added imports

--- utils/conftest.py
+++ utils/conftest.py
@@ -1,4 +1,6 @@
-
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
 

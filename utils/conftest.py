cbf6b45b4b6278b72b9ba978c9b65349933a8e77
yahya09206 <yahyahussein91@gmail.com>
2025-08-18 18:21:35 -0700

    setting up fixture

--- utils/conftest.py
+++ utils/conftest.py
@@ -4,3 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+
+@pytest.fixture(scope="function")
+def driver():

6f767fca1f218ee6c419afaa53d7c3f0d0cf619c
yahya09206 <yahyahussein91@gmail.com>
2025-08-18 18:31:39 -0700

    forgot to add self

---
+++ utils/test_base.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        """Setup WebDriver before each test."""
+        browser = os.getenv("BROWSER", "chrome").lower() # default will = chrome
+
+        if browser == "chrome":
+            chrome_options = ChromeOptions()
+            chrome_options.add_argument("--headless=new")
+            self.driver = webdriver.Chrome(
+                service=ChromeService(ChromeDriverManager().install()),
+                options=chrome_options
+            )
+
+

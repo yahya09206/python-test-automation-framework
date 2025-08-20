a8251fcd3fe43bd7daef6c29b29589d8040a2ec3
yahya09206 <yahyahussein91@gmail.com>
2025-08-19 18:37:01 -0700

    set up test to check title of yahoo home page

--- tests/day06_pytest_utility_methods/yahoo_search_test.py
+++ tests/day06_pytest_utility_methods/yahoo_search_test.py
@@ -1,4 +1,10 @@
+from day02_locators.get_element_attribute import driver
 from utils.test_base import TestBase
 
 
-
+class YahooSearchTest(TestBase):
+    def test_yahoo_search_home_page(self):
+        driver.get("https://search.yahoo.com")
+        expected_title = "Yahoo Search - Web Search"
+        actual_title = driver.title
+        assert expected_title == actual_title

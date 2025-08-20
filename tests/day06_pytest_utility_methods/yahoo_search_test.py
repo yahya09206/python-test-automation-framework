4b16cbbe6618663be264a5de4f99751e95e24c84
yahya09206 <yahyahussein91@gmail.com>
2025-08-19 18:41:55 -0700

    need to fix

--- tests/day06_pytest_utility_methods/yahoo_search_test.py
+++ tests/day06_pytest_utility_methods/yahoo_search_test.py
@@ -4,7 +4,7 @@ from utils.test_base import TestBase
 
 class YahooSearchTest(TestBase):
     def test_yahoo_search_home_page(self):
-        driver.get("https://search.yahoo.com")
+        self.driver.get("https://search.yahoo.com")
         expected_title = "Yahoo Search - Web Search"
         actual_title = driver.title
         assert expected_title == actual_title

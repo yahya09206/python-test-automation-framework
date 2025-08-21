cff6476d8d7c792b001f6d3b01cc9d9732627f48
yahya09206 <yahyahussein91@gmail.com>
2025-08-20 23:02:39 -0700

    tried a different assert but it did not work

--- tests/day06_pytest_utility_methods/yahoo_search_test.py
+++ tests/day06_pytest_utility_methods/yahoo_search_test.py
@@ -6,5 +6,4 @@ class YahooSearchTest(TestBase):
     def test_yahoo_search_home_page(self):
         self.driver.get("https://search.yahoo.com")
         expected_title = "Yahoo Search - Web Search"
-        actual_title = driver.title
-        assert expected_title == actual_title
+        assert expected_title == driver.title, f"Expected '{expected_title}' but got '{driver.title}'"

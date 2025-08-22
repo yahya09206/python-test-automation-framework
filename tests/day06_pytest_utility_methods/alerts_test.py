40b24157e37da0d87ce15f5e9e9b6cd55a64dd9a
yahya09206 <yahyahussein91@gmail.com>
2025-08-22 11:55:33 -0700

    put baby to nap

---
+++ tests/day06_pytest_utility_methods/alerts_test.py
@@ -0,0 +1,8 @@
+from utils.test_base import TestBase
+from selenium.webdriver.common.by import By
+
+class AlertsTest(TestBase):
+    def test_for_alerts(self):
+        self.driver.get("https://practice.cydeo.com/javascript_alerts")
+
+        self.driver.find_element(By.)

245f2bb792bd7290ec800a71a7d3bebd884fb6cc
yahya09206 <yahyahussein91@gmail.com>
2025-08-21 22:37:15 -0700

    printing to the log to see why a different site is opening up

--- tests/day06_pytest_utility_methods/yahoo_search_test.py
+++ tests/day06_pytest_utility_methods/yahoo_search_test.py
@@ -4,6 +4,9 @@ from utils.test_base import TestBase
 
 class YahooSearchTest(TestBase):
     def test_yahoo_search_home_page(self):
+        print("Initial URL:", self.driver.current_url)
         self.driver.get("https://search.yahoo.com")
-        expected_title = "Yahoo Search - Web Search"
-        assert expected_title == self.driver.title, f"Expected '{expected_title}' but got '{driver.title}'"
+        print("After navigation URL:", self.driver.current_url)
+        expected_title = "Yahoos Search - Web Search"
+        actual_title = self.driver.title
+        assert "Yahoo Search" in actual_title, f"{expected_title} in title but got {actual_title}"

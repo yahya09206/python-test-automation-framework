d8cf644e5dceb83768eaa06ae829823aee1f8130
yahya09206 <yahyahussein91@gmail.com>
2025-08-23 19:42:42 -0700

    added waits to see if text was being entered

--- tests/day06_pytest_utility_methods/alerts_test.py
+++ tests/day06_pytest_utility_methods/alerts_test.py
@@ -19,4 +19,5 @@ class AlertsTest(TestBase):
         self.driver.find_element(By.XPATH, "//button[.='Click for JS Prompt']").click()
         time.sleep(2)
         self.driver.switch_to.alert.send_keys("Hello")
+        time.sleep(2)
         self.driver.switch_to.alert.accept()

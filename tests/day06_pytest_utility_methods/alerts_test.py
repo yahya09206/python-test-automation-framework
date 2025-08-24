4efd291d1c1737933f3eef6949b06794285f7a83
yahya09206 <yahyahussein91@gmail.com>
2025-08-23 19:33:48 -0700

    text was printed after adding a 2 second wait

--- tests/day06_pytest_utility_methods/alerts_test.py
+++ tests/day06_pytest_utility_methods/alerts_test.py
@@ -5,11 +5,13 @@ from selenium.webdriver.common.by import By
 
 class AlertsTest(TestBase):
     def test_for_alerts(self):
+
         self.driver.get("https://practice.cydeo.com/javascript_alerts")
 
         self.driver.find_element(By.XPATH, "//button[.='Click for JS Alert']").click()
 
         time.sleep(2)
-
         print(self.driver.switch_to.alert.text)
+        time.sleep(2)
+
 

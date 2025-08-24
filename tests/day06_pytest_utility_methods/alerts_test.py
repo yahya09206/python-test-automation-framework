5b0ba89cc9d45086f7a28e7cb2e7620c280f92e7
yahya09206 <yahyahussein91@gmail.com>
2025-08-23 19:21:43 -0700

    clicking on first alert box

--- tests/day06_pytest_utility_methods/alerts_test.py
+++ tests/day06_pytest_utility_methods/alerts_test.py
@@ -5,4 +5,4 @@ class AlertsTest(TestBase):
     def test_for_alerts(self):
         self.driver.get("https://practice.cydeo.com/javascript_alerts")
 
-        self.driver.find_element(By.)
+        self.driver.find_element(By.XPATH, "//button[.='Click for JS Alert']").click()

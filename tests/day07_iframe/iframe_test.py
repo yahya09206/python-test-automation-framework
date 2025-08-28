b41ec2a007446aef85c2581da2ed3f305cdd9887
yahya09206 <yahyahussein91@gmail.com>
2025-08-27 21:12:58 -0700

    locating p tag inside of iframe

--- tests/day07_iframe/iframe_test.py
+++ tests/day07_iframe/iframe_test.py
@@ -1,12 +1,15 @@
-from tests.day05_css_xpath_pytest.test_title import driver
 from utils.test_base import TestBase
 
 from selenium.webdriver.common.by import By
 
-class IframTest(TestBase):
+class IframeTest(TestBase):
     def test_for_iframe(self):
         self.driver.get("https://practice.cydeo.com/iframe")
 
         iframe_element = self.driver.find_element(By.CSS_SELECTOR, "iframe[title^='Rich Text Area']")
         # switch to iframe element
         self.driver.switch_to.frame(iframe_element)
+
+        # locate p tag inside of iframe
+        paragraph_tag = self.driver.find_element(By.TAG_NAME, "p")
+        print(paragraph_tag.text)

807571caa773f2402977db7b68c7375858d07644
yahya09206 <yahyahussein91@gmail.com>
2025-08-27 21:11:23 -0700

    locating iframe element and switching to it

--- tests/day07_iframe/iframe_test.py
+++ tests/day07_iframe/iframe_test.py
@@ -1,6 +1,12 @@
+from tests.day05_css_xpath_pytest.test_title import driver
 from utils.test_base import TestBase
 
+from selenium.webdriver.common.by import By
 
 class IframTest(TestBase):
     def test_for_iframe(self):
         self.driver.get("https://practice.cydeo.com/iframe")
+
+        iframe_element = self.driver.find_element(By.CSS_SELECTOR, "iframe[title^='Rich Text Area']")
+        # switch to iframe element
+        self.driver.switch_to.frame(iframe_element)

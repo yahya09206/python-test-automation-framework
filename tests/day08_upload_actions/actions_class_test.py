43cd24fc95960423ede5327f67e81bc7cddb5f8d
yahya09206 <yahyahussein91@gmail.com>
2025-09-01 21:07:25 -0700

    locating first image to hover over

--- tests/day08_upload_actions/actions_class_test.py
+++ tests/day08_upload_actions/actions_class_test.py
@@ -1,5 +1,8 @@
 from utils.test_base import TestBase
 
+from selenium.webdriver.common.by import By
+
 
 class ActionsClassTest(TestBase):
     def test_for_actions_class(self):
+        image_element_one = self.driver.find_element(By.XPATH, "//div[@class='figure']/img[1]")

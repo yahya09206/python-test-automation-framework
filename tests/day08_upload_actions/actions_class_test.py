e2826b9278dceca22eecfc6ea973b3a0fd646f84
yahya09206 <yahyahussein91@gmail.com>
2025-09-01 21:09:14 -0700

    imported actionchains class

--- tests/day08_upload_actions/actions_class_test.py
+++ tests/day08_upload_actions/actions_class_test.py
@@ -1,3 +1,5 @@
+from selenium.webdriver import ActionChains
+
 from utils.test_base import TestBase
 
 from selenium.webdriver.common.by import By
@@ -6,3 +8,6 @@ from selenium.webdriver.common.by import By
 class ActionsClassTest(TestBase):
     def test_for_actions_class(self):
         image_element_one = self.driver.find_element(By.XPATH, "//div[@class='figure']/img[1]")
+
+        # create instance of Actions class by passing driver object
+        actions = ActionChains(self.driver)

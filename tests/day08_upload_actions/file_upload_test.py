1eb27be28b8d000989d0c3b89a8bdb71349d8af5
yahya09206 <yahyahussein91@gmail.com>
2025-09-02 21:59:04 -0700

    cliking on upload button

--- tests/day08_upload_actions/file_upload_test.py
+++ tests/day08_upload_actions/file_upload_test.py
@@ -9,3 +9,5 @@ class FileUpload(TestBase):
 
         file_path = "/Users/yahyahussein91/Desktop/photo-1756474215990-a18a9a0521d5.avif"
         file_input_field.send_keys(file_path)
+
+        self.driver.find_element(By.XPATH, "//input[@id='file-submit']").click()

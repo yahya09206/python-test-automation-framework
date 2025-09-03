25fa36505d31b8217c3100fb5625744df9f9c46f
yahya09206 <yahyahussein91@gmail.com>
2025-09-02 21:57:56 -0700

    uploading file into input field

--- tests/day08_upload_actions/file_upload_test.py
+++ tests/day08_upload_actions/file_upload_test.py
@@ -8,3 +8,4 @@ class FileUpload(TestBase):
         file_input_field = self.driver.find_element(By.ID, "file-upload")
 
         file_path = "/Users/yahyahussein91/Desktop/photo-1756474215990-a18a9a0521d5.avif"
+        file_input_field.send_keys(file_path)

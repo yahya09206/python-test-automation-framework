30f4ee7e26588e31a66c3efadfd3fdd5e94ed045
yahya09206 <yahyahussein91@gmail.com>
2025-09-04 13:32:02 -0700

    clicking element by partial link

--- tests/day09_explicitwait_singleton_properties/explicit_wait_test.py
+++ tests/day09_explicitwait_singleton_properties/explicit_wait_test.py
@@ -1,5 +1,10 @@
 from utils.test_base import TestBase
 
+from selenium.webdriver.common.by import By
+
 
 class ExplicitWaitTest(TestBase):
     def test_for_explicit_wait(self):
+        self.driver.get("practice.cydeo.com/dynamic_controls")
+
+        self.driver.find_element(By.PARTIAL_LINK_TEXT, "Example 7").click()

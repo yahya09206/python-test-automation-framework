5d74e06f2d295729b95024e6ca20b7b36999f494
yahya09206 <yahyahussein91@gmail.com>
2025-09-04 13:41:32 -0700

    still working on it

--- tests/day09_explicitwait_singleton_properties/explicit_wait_test.py
+++ tests/day09_explicitwait_singleton_properties/explicit_wait_test.py
@@ -8,11 +8,11 @@ from selenium.webdriver.common.by import By
 
 class ExplicitWaitTest(TestBase):
     def test_for_explicit_wait(self):
-        self.driver.get("practice.cydeo.com/dynamic_controls")
+        self.driver.get("https://practice.cydeo.com/dynamic_loading")
 
         self.driver.find_element(By.PARTIAL_LINK_TEXT, "Example 7").click()
 
         wait = WebDriverWait(self.driver, 10)
-        wait.until(visibility_of_element_located(By.XPATH, "//img[@alt='square pants']"))
+        element = wait.until(visibility_of_element_located(By.XPATH, "//img[@alt='square pants']"))
 
         print("THE END")

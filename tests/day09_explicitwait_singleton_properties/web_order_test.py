a8b5ed5a7e0872c7a944acd29a075a857a84f741
yahya09206 <yahyahussein91@gmail.com>
2025-09-08 18:14:39 -0700

    setting driver to fetch site

--- tests/day09_explicitwait_singleton_properties/web_order_test.py
+++ tests/day09_explicitwait_singleton_properties/web_order_test.py
@@ -3,3 +3,4 @@ from utils.test_base import TestBase
 
 class WebOrderTest(TestBase):
     def test_web_order(self):
+        self.driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx")

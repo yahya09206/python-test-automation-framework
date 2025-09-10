30fe5e123c792f7776f7565369366f2d27110203
yahya09206 <yahyahussein91@gmail.com>
2025-09-09 19:36:56 -0700

    needed to import util

--- tests/day09_explicitwait_singleton_properties/web_order_test.py
+++ tests/day09_explicitwait_singleton_properties/web_order_test.py
@@ -5,16 +5,18 @@ from datetime import time
 from selenium.webdriver.common.by import By
 
 from utils.test_base import TestBase
+from utils.web_order_util import WebOrderUtil
 
 
 class WebOrderTest(TestBase):
     def test_web_order(self):
         self.driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx")
 
-        web
-
         self.driver.find_element(By.ID, "ctl00_MainContent_username").send_keys("Tester")
         self.driver.find_element(By.ID, "ctl00_MainContent_password").send_keys("test")
         self.driver.find_element(By.ID, "ctl00_MainContent_login_button").click()
 
-        time.sleep(3)
+
+
+    def test_web_order_method(self):
+        WebOrderUtil.open_web_order_app(self.driver)

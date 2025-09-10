1d569bb9df028b953a32bd98cdd1bb6d84b0d1ef
yahya09206 <yahyahussein91@gmail.com>
2025-09-09 19:21:08 -0700

    navigating to smart bear website

--- utils/web_order_util.py
+++ utils/web_order_util.py
@@ -6,3 +6,4 @@ class WebOrderUtil:
     @staticmethod
     def open_web_order_app(driver):
         """Open the WebOrder application"""
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx")
